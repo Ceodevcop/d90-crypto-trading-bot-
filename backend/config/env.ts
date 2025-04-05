9f758edba572c845a0deb45763ba298c342e1d0e
Ahmad Hamza <priahmz@gmail.com>
2025-04-05 16:05:44 +0100

    Create env.ts

---
+++ backend/config/env.ts
@@ -0,0 +1,9 @@
+export const BINANCE_CONFIG = {
+  API_KEY: 'bg_ffcbb26a743c6f3617a03e4edb87aa3f',
+  API_SECRET: 'e397e3420dbb6a1b48dfef734e6ef8d6aaf29ee44a044d51dd1742a8143c0693'
+};
+
+export const GRID_CONFIG = {
+  DEFAULT_SPACING: 0.5, // 0.5% between grid levels
+  MAX_GRID_LEVELS: 100
+};
